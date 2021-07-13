12b91bc1c059c1f5607bd68d828e417a17800067
Stefan Kulik <stefan.kulik@t-online.de>
2021-07-13 17:11:11 +0200

    placeholder -> tanzflaeche bilder, erstmal fertig

--- src/main/resources/static/assets/js/script.js
+++ src/main/resources/static/assets/js/script.js
@@ -3,6 +3,26 @@ function submitPot() {
 }
 
 
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("bild");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+}
+
+
+
 /*<![CDATA[*/
 var app = new Vue({
     el: '#body',
